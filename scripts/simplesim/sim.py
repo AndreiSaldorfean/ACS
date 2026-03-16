3ac8a3b31b1f190a56ca958cd7262bc755c19d28
MN3141 <mihai.neag@ulbsibiu.ro>
2026-03-17 00:11:16 +0200

    Added basic logic for driver script

--- scripts/simplesim/sim.py
+++ scripts/simplesim/sim.py
@@ -1,29 +1,67 @@
 import json
 import os
+import subprocess
+import sys
 
-benchmark_cfg = []
+script_path = os.path.abspath(os.path.dirname(__file__))
+
 
 def get_input():
 
     file_name = "config.json"
-    file_name = os.path.join(os.getcwd(),file_name)
+    file_name = os.path.join(script_path, file_name)
     file_data = []
 
     try:
         with open(file_name) as file_handle:
-            file_data = json.loads(file_handle)
+            file_data = json.load(file_handle)
     except:
         print(f"Cannot open {file_name}!")
+        sys.exit(1)
 
     return file_data
 
-def get_command():
-    pass
 
-def run_command():
-    pass
+def get_commands(benchmark_cfg: dict):
+
+    sim_root = os.path.join(script_path, "..", "..", "simplesim-3.0")
+    out_dir_root = os.path.join(script_path, "..", "..", "build")
+    commands = []
+
+    os.makedirs(out_dir_root, exist_ok=True)
+
+    for benchmark in benchmark_cfg:
+
+        benchmark_dir = os.path.join(out_dir_root, benchmark)
+        os.makedirs(benchmark_dir, exist_ok=True)
+
+        for sim in benchmark_cfg[benchmark]:
+
+            cmd = f"{sim_root}/sim-{sim}"
+            for config in benchmark_cfg[benchmark][sim]:
+
+                cfg_value = benchmark_cfg[benchmark][sim][config]
+                cfg_name = f"{benchmark}_{sim}_{config}"
+                sim_out = os.path.join(benchmark_dir, cfg_name, "simout.res")
+                prog_out = os.path.join(benchmark_dir, cfg_name, "progout.res")
+
+                cmd = f"{cmd} -redir:sim {sim_out} -redir:prog {prog_out} {cfg_value}"
+                commands.append(cmd)
+
+    return commands
+
+
+def run_command(command: str):
+
+    try:
+        result = subprocess.run(command,shell=True)
+    except:
+        print(f"Cannot run command {command}!")
+        sys.exit(1)
 
 def main():
+
     benchmark_cfg = get_input()
+    commands = get_commands(benchmark_cfg)
 
 main()
