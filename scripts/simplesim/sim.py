1195c1c1f20bbfe354ad8ff699073e705e3579cd
MN3141 <mihai.neag@ulbsibiu.ro>
2026-03-21 13:27:12 +0200

    Added logging and basic command generation and execution

--- scripts/simplesim/sim.py
+++ scripts/simplesim/sim.py
@@ -1,9 +1,18 @@
+import shutil
 import json
 import os
 import subprocess
 import sys
 
 script_path = os.path.abspath(os.path.dirname(__file__))
+out_dir_root = os.path.join(script_path, "..", "..", "build")
+cmd_log_file = os.path.join(out_dir_root, "cmd_run.log")
+
+
+def cleanup():
+
+    if os.path.exists(out_dir_root):
+        shutil.rmtree(out_dir_root)
 
 
 def get_input():
@@ -25,27 +34,28 @@ def get_input():
 def get_commands(benchmark_cfg: dict):
 
     sim_root = os.path.join(script_path, "..", "..", "simplesim-3.0")
-    out_dir_root = os.path.join(script_path, "..", "..", "build")
     commands = []
 
     os.makedirs(out_dir_root, exist_ok=True)
 
     for benchmark in benchmark_cfg:
 
+        executable_file = benchmark_cfg[benchmark]["executable_file"]
+        input_file = benchmark_cfg[benchmark]["input"]
+        max_instructions = benchmark_cfg[benchmark]["max_instructions"]
         benchmark_dir = os.path.join(out_dir_root, benchmark)
+
         os.makedirs(benchmark_dir, exist_ok=True)
 
-        for sim in benchmark_cfg[benchmark]:
+        for sim in benchmark_cfg[benchmark]["simulators"]:
 
-            cmd = f"{sim_root}/sim-{sim}"
-            for config in benchmark_cfg[benchmark][sim]:
+            for config in benchmark_cfg[benchmark]["simulators"][sim]:
 
-                cfg_value = benchmark_cfg[benchmark][sim][config]
+                cfg_value = benchmark_cfg[benchmark]["simulators"][sim][config]
                 cfg_name = f"{benchmark}_{sim}_{config}"
-                sim_out = os.path.join(benchmark_dir, cfg_name, "simout.res")
-                prog_out = os.path.join(benchmark_dir, cfg_name, "progout.res")
-
-                cmd = f"{cmd} -redir:sim {sim_out} -redir:prog {prog_out} {cfg_value}"
+                sim_out = os.path.join(benchmark_dir, f"{cfg_name}_simout.res")
+                prog_out = os.path.join(benchmark_dir, f"{cfg_name}_progout.res")
+                cmd = f"{sim_root}/sim-{sim} -redir:sim {sim_out} -redir:prog {prog_out} -max:inst {max_instructions} {cfg_value} {executable_file} {input_file}"
                 commands.append(cmd)
 
     return commands
@@ -54,14 +64,29 @@ def get_commands(benchmark_cfg: dict):
 def run_command(command: str):
 
     try:
-        result = subprocess.run(command,shell=True)
-    except:
-        print(f"Cannot run command {command}!")
+        with open(cmd_log_file, "a") as file_handle:
+
+            result = subprocess.run(
+                command, shell=True, stdout=file_handle, stderr=file_handle
+            )
+            file_handle.write(f"Ran {command}\n")
+            file_handle.write(f"Exit code {result.returncode}\n")
+            file_handle.write("============\n")
+
+    except Exception as e:
+        print(f"Cannot run command {command}! Error: {e}")
         sys.exit(1)
+
 
 def main():
 
+    cleanup()
+
     benchmark_cfg = get_input()
     commands = get_commands(benchmark_cfg)
+
+    for cmd in commands:
+        run_command(cmd)
+
 
 main()
