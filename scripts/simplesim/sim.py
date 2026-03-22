fa2fe61a15bcaf124717680a962bc1081d3c8d10
AndreiSaldorfean <andreisaldorfean@gmail.com>
2026-03-22 12:41:25 +0200

    feat: Add loading screen and organize build dir
    Signed-off-by: AndreiSaldorfean <andreisaldorfean@gmail.com>

--- scripts/simplesim/sim.py
+++ scripts/simplesim/sim.py
@@ -3,6 +3,10 @@ import json
 import os
 import subprocess
 import sys
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = None
 
 script_path = os.path.abspath(os.path.dirname(__file__))
 out_dir_root = os.path.join(script_path, "..", "..", "build")
@@ -24,8 +28,8 @@ def get_input():
     try:
         with open(file_name) as file_handle:
             file_data = json.load(file_handle)
-    except:
-        print(f"Cannot open {file_name}!")
+    except Exception as e:
+        print(f"Cannot open {file_name + "\nException: " + e.__str__()}!")
         sys.exit(1)
 
     return file_data
@@ -36,23 +40,21 @@ def get_commands(benchmark_cfg: dict):
     sim_root = os.path.join(script_path, "..", "..", "simplesim-3.0")
     commands = []
 
-    os.makedirs(out_dir_root, exist_ok=True)
-
     for benchmark in benchmark_cfg:
 
         executable_file = benchmark_cfg[benchmark]["executable_file"]
         input_file = benchmark_cfg[benchmark]["input"]
         max_instructions = benchmark_cfg[benchmark]["max_instructions"]
-        benchmark_dir = os.path.join(out_dir_root, benchmark)
-
-        os.makedirs(benchmark_dir, exist_ok=True)
 
         for sim in benchmark_cfg[benchmark]["simulators"]:
+            os.makedirs(out_dir_root + "/" + benchmark + "/" + sim, exist_ok=True)
+            benchmark_dir = os.path.join(out_dir_root + "/" + benchmark, sim)
+            os.makedirs(benchmark_dir, exist_ok=True)
 
             for config in benchmark_cfg[benchmark]["simulators"][sim]:
 
                 cfg_value = benchmark_cfg[benchmark]["simulators"][sim][config]
-                cfg_name = f"{benchmark}_{sim}_{config}"
+                cfg_name = config
                 sim_out = os.path.join(benchmark_dir, f"{cfg_name}_simout.res")
                 prog_out = os.path.join(benchmark_dir, f"{cfg_name}_progout.res")
                 cmd = f"{sim_root}/sim-{sim} -redir:sim {sim_out} -redir:prog {prog_out} -max:inst {max_instructions} {cfg_value} {executable_file} {input_file}"
@@ -85,7 +87,14 @@ def main():
     benchmark_cfg = get_input()
     commands = get_commands(benchmark_cfg)
 
-    for cmd in commands:
+
+    if tqdm is None:
+        print("tqdm not installed. Progress bar will not be shown. To enable, install tqdm: pip install tqdm")
+        iterator = commands
+    else:
+        iterator = tqdm(commands, desc="Running simulations", unit="sim")
+
+    for cmd in iterator:
         run_command(cmd)
 
 
