f069fe34862201f9d361c98c705c742902595edb
MN3141 <mihai.neag@ulbsibiu.ro>
2026-05-08 00:54:33 +0300

    Added support for pairs of benchmarks
    Signed-off-by: MN3141 <mihai.neag@ulbsibiu.ro>

--- scripts/msim.py
+++ scripts/msim.py
@@ -9,13 +9,36 @@ out_dir = os.path.join(script_path, "build")
 inputs_dir = os.path.join(script_path, "..", "inputs")
 benchmarks = ["applu", "equake", "galgel", "lucas", "mesa", "mgrid"]
 
+pairs = [
+    [benchmarks[0], benchmarks[1]],
+    [benchmarks[2], benchmarks[3]],
+    [benchmarks[4], benchmarks[5]],
+]
+
+triplets = [
+    [benchmarks[0], benchmarks[1], benchmarks[2]],
+    [benchmarks[3], benchmarks[4], benchmarks[5]],
+]
+
 
 def run_single_group(lvptSize):
-    global benchmarks
+
     for benchmark in benchmarks:
         arg_file_name = f"{benchmark}_{lvptSize}.arg"
         arg_file = os.path.join(inputs_dir, "args", benchmark, arg_file_name)
         cmd = f"{msim_dir}/sim-outorder -redir:sim {out_dir}/{benchmark}_{lvptSize}.res -fastfwd 1000000 -max:inst 10000000 -lvpt:size {lvptSize} {arg_file}"
+        subprocess.run(cmd, shell=True)
+
+
+def run_2_group(lvptSize):
+
+    for group in pairs:
+        arg_file_name0 = f"{group[0]}_{lvptSize}.arg"
+        arg_file_name1 = f"{group[1]}_{lvptSize}.arg"
+        arg_file0 = os.path.join(inputs_dir, "args", group[0], arg_file_name0)
+        arg_file1 = os.path.join(inputs_dir, "args", group[1], arg_file_name1)
+        cmd = f"{msim_dir}/sim-outorder -redir:sim {out_dir}/{group[0]}_{group[1]}_{lvptSize}.res -fastfwd 1000000 -max:inst 10000000 -lvpt:size {lvptSize} {arg_file0} {arg_file1}"
+
         subprocess.run(cmd, shell=True)
 
 
@@ -24,5 +47,8 @@ if __name__ == "__main__":
     lvpt_sizes = [16, 32, 64, 128, 256, 512, 1024, 2048]
     os.makedirs(out_dir, exist_ok=True)
     os.chdir(msim_dir)
-    with multiprocessing.Pool(2) as pool:
-        pool.map(run_single_group, lvpt_sizes)
+    # with multiprocessing.Pool(os.cpu_count()) as pool:
+    #     pool.map(run_single_group, lvpt_sizes)
+
+    with multiprocessing.Pool(os.cpu_count()) as pool:
+        pool.map(run_2_group, lvpt_sizes)
